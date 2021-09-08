4bfa0623769dc882b88977a664138cce696d6b0f
Antony Chechevichkin <Anton-bopoh@yandex.ru>
2021-09-08 11:28:08 +0300

    WeatherFirebase

---
+++ Weather/CityWeather/WeatherCollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherCollerctionView.swift
+//  Weather
+//
+//  Created by Антон Чечевичкин on 23.10.2020.
+//  Copyright © 2020 Антон Чечевичкин. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCollerctionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
